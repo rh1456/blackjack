aae7a487f365022be2fdc6b3f1b6e013b3f00c1f
Reid Hanson <reidhans@buffalo.edu>
2019-11-07 15:07:41 -0500

    css style

--- public/main.js
+++ public/main.js
@@ -1,7 +1,4 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+ 
 
 document.addEventListener('DOMContentLoaded', main)
