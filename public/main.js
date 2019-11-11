a6518065b4550521d858cbf9815218edfeed1f62
Reid Hanson <reidhans@buffalo.edu>
2019-11-10 23:42:06 -0500

    big updates

--- public/main.js
+++ public/main.js
@@ -15,56 +15,116 @@ const rank = [
   'Queen',
   'King'
 ]
-const value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 const deck = []
-
 const playerHand = []
+const dealerHand = []
 
-//values of card with if
+// values of card with if
+
 const makeCardValue = rank => {
   if (rank === 'Ace') {
     return 11
-  } else if (rank === 'King' || rank === 'Queen' || rank === 'jack') {
+  } else if (rank === 'King' || rank === 'Queen' || rank === 'Jack') {
     return 10
   } else {
     return parseInt(rank)
   }
 }
-const cardValue = () => {
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < rank.length; j++) {
-      const card = {
-        rank: rank[j],
-        suit: suits[i],
-        value: makeCardValue(rank[j]),
-        imageUrl: rank[j] + '_of_' + suits[i] + '.svg'
-      }
-      deck.push(card)
-    }
-  }
-  console.log([deck])
-  console.log(deck)
+// const cardValue = () => {
+//   for (let i = 0; i < suits.length; i++) {
+//     for (let j = 0; j < rank.length; j++) {
+//       const card = {
+//         rank: rank[j],
+//         suit: suits[i],
+//         value: makeCardValue(rank[j]),
+//         imageUrl: rank[j] + '_of_' + suits[i] + '.svg'
+//       }
+//       deck.push(card)
+//     }
+//   }
+//   console.log([deck])
+//   console.log(deck)
+// }
+
+// start game function
+const startGame = () => {
+  dealCardPic(deck, playerHand)
+  dealCardPic(deck, playerHand)
+  dealDealer(deck, dealerHand)
+  dealDealer(deck, dealerHand)
 }
 
-//deal and display card on UI
+// stand button=shows dealer hand
+const showDealer = () => {
+  const drawnCard = deck.pop()
+  dealerHand.push(drawnCard)
+  const cardLi = document.createElement('li')
+  const img = document.createElement('img')
+  img.src = '/images/cards/' + drawnCard.imageUrl
+  cardLi.appendChild(img)
+  document.querySelector('.dHand').appendChild(cardLi).hidden = false
+  document.querySelector('.dealer-sum').hidden = false
+}
+
+const standButton = () => {
+  showDealer()
+  showDealer()
+  dealDealer()
+}
+
+// refresh button function
+const refreshPage = () => {
+  window.location.reload()
+}
+
+// deal and display card on UI
+
 const dealCardPic = () => {
   const drawnCard = deck.pop()
   playerHand.push(drawnCard)
   const cardLi = document.createElement('li')
-  const p = document.createElement('p')
-  p.textContent = drawnCard.rank + 'of' + drawnCard.suit
+  // const p = document.createElement('p')
+  // p.textContent = drawnCard.rank + 'of' + drawnCard.suit
   const img = document.createElement('img')
   img.src = '/images/cards/' + drawnCard.imageUrl
-  cardLi.appendChild(p)
+  // cardLi.appendChild(p)
   cardLi.appendChild(img)
   document.querySelector('.pHand').appendChild(cardLi)
-
-  let sum = 0
+  let playerSum = 0
   for (let i = 0; i < playerHand.length; i++) {
-    sum += playerHand[i].value
+    playerSum += playerHand[i].value
+
+    document.querySelector('.player-sum').textContent = playerSum
   }
-  document.querySelector('.player-sum').textContent = sum
+  if (playerSum > 21) {
+    document.querySelector('.hit').disabled = true
+    document.querySelector('.start').disabled = true
+    document.querySelector('.displayP').textContent = 'Player loses'
+  } else if (playerSum <= 21) {
+    document.querySelector('.hit').disabled = false
+  }
+}
+// dealer's hand
+const dealDealer = () => {
+  const drawnCard = deck.pop()
+  dealerHand.push(drawnCard)
+  const cardLi = document.createElement('li')
+  const img = document.createElement('img')
+  img.src = '/images/cards/' + drawnCard.imageUrl
+  cardLi.appendChild(img)
+  document.querySelector('.dHand').appendChild(cardLi).hidden = true
+  let dealerSum = 0
+  for (let i = 0; i < dealerHand.length; i++) {
+    dealerSum += dealerHand[i].value
+    document.querySelector('.dealer-sum')
+  }
+  // if (dealerSum > 21) {
+  //   document.querySelector('.hit').disabled = true
+  //   document.querySelector('.start').disabled = true
+  //   document.querySelector('.displayP').textContent = 'House Loses'
+  // } else if (dealerSum <= 21) {
+  //   document.querySelector('.hit').disabled = false
+  // }
 }
 
 //create the deck
@@ -95,18 +155,19 @@ const shuffleDeck = () => {
   // console.log(shuffleDeck)
 }
 
-const dealCard = () => {
-  // document.querySelector('h1').textContent = deck[0]
-  document.querySelector('.card').textContent = deck.splice(
-    deck.length - 1,
-    1
-  )[0]
-}
-console.log(makeDeck)
 const makeAndShuffleDeck = () => {
   makeDeck()
   shuffleDeck()
 }
 
 document.querySelector('.hit').addEventListener('click', dealCardPic)
+document
+  .querySelector('.start')
+  .addEventListener('click', startGame, { once: true })
+document.querySelector('.resetButton').addEventListener('click', refreshPage)
+document.querySelector('.stay').addEventListener('click', standButton)
+
+// document.querySelector('.hit').addEventListener('click', dealDealer)
+// document.querySelector('.stay').addEventListener('click', disableHit)
+
 document.addEventListener('DOMContentLoaded', makeAndShuffleDeck)
